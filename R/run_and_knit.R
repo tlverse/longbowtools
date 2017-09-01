38486185115f6b40d76c502196033c6f411078d0
Jeremy Coyle <jeremyrcoyle@gmail.com>
2017-09-01 15:42:45 -0700

    fix pandoc weirdness regarding paths referencing ~

--- R/run_and_knit.R
+++ R/run_and_knit.R
@@ -2,19 +2,20 @@
 #' @export
 #' @importFrom knitr knit
 run_and_knit <- function(Rmd_file, tlparams, run_pandoc=FALSE){
-  
-  output_Rmd_file <- file.path(tlparams$output_dir, "REPORT.Rmd")
-  file.copy(Rmd_file, output_Rmd_file)
-  md_file <- file.path(tlparams$output_dir, "REPORT.md")
+  Rmd_file_name <- "REPORT.Rmd"
+  md_file_name <- "REPORT.md"
+  output_Rmd_path <- file.path(tlparams$output_dir, Rmd_file_name)
+  file.copy(Rmd_file, output_Rmd_path, overwrite = TRUE)
+  md_file <- file.path(tlparams$output_dir, "md_file_name")
   
   # knitr is sensitive to working directory
   # see https://github.com/yihui/knitr/issues/913#issuecomment-66897715
   owd <- setwd(tlparams$output_dir)
   try({
-    knit(output_Rmd_file,output=md_file)
+    knit(Rmd_file_name,output=md_file_name)
     
     if(run_pandoc){
-      pandoc(md_file)
+      pandoc(md_file_name)
     }
   })
   
