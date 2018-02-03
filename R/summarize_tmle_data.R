ae2c8af6aa3d3c8568b0f109111f9ccd749fe417
Jeremy Coyle <jeremyrcoyle@gmail.com>
2018-02-03 13:28:31 -0800

    improve data summaries

--- R/summarize_tmle_data.R
+++ R/summarize_tmle_data.R
@@ -10,6 +10,13 @@ infer_variable_types <- function(data){
                            ifelse(level_counts<10, "factor", "numeric"))
 }
 
+roles_from_node_list <- function(variables, node_list){
+  all_nodes <- unlist(node_list)
+  all_roles <- rep(names(node_list), sapply(node_list, length))
+  roles <- all_roles[match(variables, all_nodes)]
+  
+  return(roles)
+}
 
 #' @export
 #' 
@@ -17,7 +24,18 @@ infer_variable_types <- function(data){
 summarize_tmle_data <- function(data, node_list){
   tmle_data <- data[, unlist(node_list), with=FALSE]
   variable_types <- infer_variable_types(tmle_data)
-  c1 <- skim(tmle_data, variable_types)
-
-  return(c1)
+  
+    
+  # include extra role variable in output
+  skim_list <- skim_to_list(tmle_data, variable_types)
+  role_order <- names(node_list)
+  add_roles_to_skim_table <- function(skim_table){
+    roles <- roles_from_node_list(skim_table$variable, node_list)
+    skim_table <- cbind(role = roles, skim_table)
+    skim_table[order(match(skim_table$role, role_order), skim_table$variable), ]
+  }
+  
+  skim_list <- lapply(skim_list, add_roles_to_skim_table)
+  
+  return(skim_list)
 }
